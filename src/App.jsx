d6f1264746403ca37dadd5fcb20b546a4ca7fd83
StarKnightt <prasenjitt4e@gmail.com>
2024-07-16 23:23:35 +0530

    added functionallity to our calculator and added some functions to make it work

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,64 @@
-import './App.css'
-import React from 'react'
+import './App.css';
+import React, { useReducer } from 'react';
+import DigitButton from './DigitButton.jsx';
+import OperationButton from './OperationButton.jsx';
+
+export const ACTIONS = {
+  ADD_DIGIT: 'add-digit',
+  CHOOSE_OPERATION: 'choose-operation',
+  CLEAR: 'clear',
+  DELETE_DIGIT: 'delete-digit',
+  EVALUATE: 'evaluate'
+};
+
+function reducer(state, { type, payload }) {
+  switch (type) {
+    case ACTIONS.ADD_DIGIT:
+      return {
+        ...state,
+        currentOperand: `${state.currentOperand || ''}${payload.digit}`
+      };
+    case ACTIONS.CHOOSE_OPERATION:
+      return {
+        ...state,
+        operation: payload.operation
+      };
+    default:
+      return state;
+  }
+}
+
 function App() {
+  const [{ currentOperand, previousOperand, operation }, dispatch] = useReducer(reducer, {});
 
   return (
     <>
       <div className="calculator-grid">
         <div className="output">
-          <div className="previous-operand">123,234*</div>
-          <div className="current-operand">123,234</div>
+          <div className="previous-operand">{previousOperand} {operation}</div>
+          <div className="current-operand">{currentOperand}</div>
         </div>
         <button className="span-two">AC</button>
         <button>DEL</button>
-        <button>/</button>
-        <button>1</button>
-        <button>2</button>
-        <button>3</button>
-        <button>X</button>
-        <button>4</button>
-        <button>5</button>
-        <button>6</button>
-        <button>+</button>
-        <button>7</button>
-        <button>8</button>
-        <button>9</button>
-        <button>-</button>
-        <button>.</button>
-        <button>0</button>
+        <OperationButton operation="/" dispatch={dispatch} />
+        <DigitButton digit="1" dispatch={dispatch} />
+        <DigitButton digit="2" dispatch={dispatch} />
+        <DigitButton digit="3" dispatch={dispatch} />
+        <OperationButton operation="*" dispatch={dispatch} />
+        <DigitButton digit="4" dispatch={dispatch} />
+        <DigitButton digit="5" dispatch={dispatch} />
+        <DigitButton digit="6" dispatch={dispatch} />
+        <OperationButton operation="+" dispatch={dispatch} />
+        <DigitButton digit="7" dispatch={dispatch} />
+        <DigitButton digit="8" dispatch={dispatch} />
+        <DigitButton digit="9" dispatch={dispatch} />
+        <OperationButton operation="-" dispatch={dispatch} />
+        <DigitButton digit="." dispatch={dispatch} />
+        <DigitButton digit="0" dispatch={dispatch} />
         <button className="span-two">=</button>
       </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/DigitButton.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { ACTIONS } from './App';
+
+export default function DigitButton({ dispatch, digit }) {
+  return (
+    <button onClick={() => dispatch({ type: ACTIONS.ADD_DIGIT, payload: { digit } })}>
+      {digit}
+    </button>
+  );
+}
